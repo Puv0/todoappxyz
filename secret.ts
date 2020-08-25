651aa3842cea34edf6dec7627caa4974737dced0
Puv0 <cagilhansozer@gmail.com>
2020-08-26 02:40:01 +0300

    please

---
+++ secret.ts
@@ -0,0 +1,5 @@
+export default {
+    mongoUrl:'mongodb+srv://puv0:YrXC8HBBuNz7kf1j@cluster0.533rj.mongodb.net/cluster0?retryWrites=true&w=majority',
+    myPlaintextPassword: 's0/\/\P4$$w0rD',
+    jwTtext:'something',
+}
