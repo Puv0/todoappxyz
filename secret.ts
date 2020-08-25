fc4d36350f260a38264369ef9ebc391572ce49f1
Puv0 <cagilhansozer@gmail.com>
2020-08-26 02:46:31 +0300

    heroku-deploy

--- secret.ts
+++
@@ -1,5 +0,0 @@
-export default {
-    mongoUrl:'mongodb+srv://puv0:YrXC8HBBuNz7kf1j@cluster0.533rj.mongodb.net/cluster0?retryWrites=true&w=majority',
-    myPlaintextPassword: 's0/\/\P4$$w0rD',
-    jwTtext:'something',
-}
